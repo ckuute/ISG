dda68e345a13e6f2970d0626e7604e62e2b9f48c
ckuute <110961321+ckuute@users.noreply.github.com>
2023-04-27 23:41:22 +0800

    Add files via upload

---
+++ FilesToVideo/FilesToVideo.py
@@ -0,0 +1,112 @@
+import numpy as np
+import cv2
+import glob
+import time
+import os
+import shutil
+from PIL import Image
+
+
+w = 1280  # x
+h = 720  # y
+frameSize = (w, h)
+
+n="WinMerge-2.16.28-Setup.exe"
+file_path = "C:/Users/william/Downloads/"+n
+
+def creatdir(path):
+    isExists=os.path.exists(path)
+    if not isExists:
+        os.makedirs(path)
+    else:
+        shutil.rmtree(path)
+        os.makedirs(path)
+        
+
+
+def FTP(filepath):
+    
+    file_name = filepath.split("/")[-1]
+    path = "C:/ISG/FilesToVideo/images/"
+    creatdir(path+file_name)
+    
+
+
+    with open(filepath, "rb") as f:
+        tt = (255, 255, 255)
+        ff = (0, 0, 0)
+        t = 0
+
+        while True:
+            file = f.read(115200)
+            if not file:
+                break
+            file_int = int.from_bytes(file, byteorder='little')
+            file_bits = bin(file_int)[2:].zfill(len(file)*8)
+
+            if len(file_bits) < 921600:
+                last_frame_pnum = len(file_bits)
+
+
+            print("now is "+str(t)+".png")
+            image = Image.new(mode="RGB", size=(w, h))
+            img=image.load()
+            i=0
+            for x in range(w):
+                for y in range(0,h,1):
+                    try:
+                        k = file_bits[i]
+                    except:
+                        break
+                    if k == "1":
+                        img[x, y] = tt
+                    # img[x, y+1]=tt
+                    # img[x, y+2] = tt
+                    # img[x, y+3]=tt
+                    # img[x, y+4] = tt
+                    # img[x, y+5]=tt
+                    # img[x, y+6] = tt
+                    # img[x, y+7]=tt
+                    else:
+                        img[x, y] = ff
+                    # img[x, y+1] = ff
+                    # img[x, y+2] = ff
+                    # img[x, y+3]=ff
+                    # img[x, y+4] = ff
+                    # img[x, y+5] = ff
+                    # img[x, y+6] = ff
+                    # img[x, y+7]=ff
+                    i+=1
+
+            
+            image.save(path+file_name+"/"+str(t)+".png")
+            t += 1
+        
+        if t<30:
+            k=30-t
+            image1 = Image.new(mode="RGB", size=(w, h))
+            for i in range(k):
+                image1.save(path+file_name+"/"+str(t+i)+".png")
+                
+
+        return file_name,last_frame_pnum,t
+
+
+def PTV(file_name,last_frame_pnum,t):
+    vedio_name = file_name+","+str(t)+","+str(last_frame_pnum)
+    creatdir('C:/ISG/FilesToVideo/videos/'+file_name)
+    out = cv2.VideoWriter('C:/ISG/FilesToVideo/videos/'+file_name+"/"+vedio_name+'.avi', cv2.VideoWriter_fourcc(*'XVID'), 30, frameSize)
+    if t<30:
+        k=30
+    else:
+        k=t
+    for filenum in range(k):
+        print(filenum)
+        img = cv2.imread('C:/ISG/FilesToVideo/images/'+file_name+"/"+str(filenum)+'.png')
+        out.write(img)
+    out.release()
+    # shutil.rmtree('C:/ISG/FilesToVideo/images/'+file_name)
+
+file_name,last_frame_pnum,t=FTP(file_path)
+PTV(file_name,last_frame_pnum,t)
+print("done")
